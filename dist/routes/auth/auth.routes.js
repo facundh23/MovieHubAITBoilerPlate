6e8e6fc1549e06ae958154cda51bc0cc14cfe47b
Facundo Herrera <facundhfed@gmail.com>
2023-10-27 15:33:15 +0200

    Building

--- dist/routes/auth/auth.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("../../controller/user/user.controller");
-const authRouter = (0, express_1.Router)();
-authRouter.post("/signup", user_controller_1.signUp);
-exports.default = authRouter;
